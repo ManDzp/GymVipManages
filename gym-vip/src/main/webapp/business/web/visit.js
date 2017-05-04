5620af13b7fb42c878475090963b1017df276b90
jingjiqiang <jingjq@yawei.com.cn>
2017-05-04 23:28:47 +0800

    移除冗余文件

--- gym-vip/src/main/webapp/business/web/visit.js
+++
@@ -1,8 +0,0 @@
-// 记录论坛浏览量
-$(function() {
-	$.post(glogbalData.contextPath + "/webindex/visit", {
-		"objectguid" : $("#objectGuid").val(),
-		"objecttype" : $("#objectType").val()
-	}, function(data) {
-	});
-});
